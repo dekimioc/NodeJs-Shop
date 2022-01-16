a7040d69b9e5cbb48c1f3c96016e678e4c85d8df
dekimioc <dekimioc@yahoo.com>
2022-01-16 20:21:54 +0100

    template engine ejs

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '/views', '404.html'))
+    res.status(404).render('404', {
+        pageTitle: '404 Not Found',
+        path: ''
+    });
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,22 @@ const path = require('path');
 const router = express.Router();
 
 const rootDir = require('../utils/path');
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
 })
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title})
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products; 
